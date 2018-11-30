b320aa4df9d61bfde178e9978168cd1d384b1c80
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2018-11-30 17:17:47 +0100

    Adding deploying script

---
+++ deploy-docs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+
+VERSION=$1
+BRANCH="${VERSION}.x.x"
+SITE_FOLDER_NAME="v${VERSION}"
+
+# Me muevo a la rama de la version
+git checkout $BRANCH
+git pull
+
+# Genero carpeta _site
+# bundle exec jekyll build --config _config.yml,_config.deploy.yml
+
+# Renombro _site a vX
+# mv _site $SITE_FOLDER_NAME
+
+#git add $SITE_FOLDER_NAME
+#git commit -m "Updating ${BRANCH} version docs"
+#git merge -X ours gh-pages
