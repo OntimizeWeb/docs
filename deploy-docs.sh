01c7a6939d689d9a0efdf0192751ec9fd9f625c8
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2018-12-03 09:41:37 +0100

    Updating deploying script

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -4,17 +4,24 @@ clear
 VERSION=$1
 BRANCH="${VERSION}.x.x"
 SITE_FOLDER_NAME="v${VERSION}"
+GHPAGES="gh-pages"
 
 # Me muevo a la rama de la version
 git checkout $BRANCH
 git pull
 
 # Genero carpeta _site
-# bundle exec jekyll build --config _config.yml,_config.deploy.yml
+bundle exec jekyll build --config _config.yml,_config.deploy.yml
 
 # Renombro _site a ../vX
-# mv _site ../$SITE_FOLDER_NAME
+mv _site ../$SITE_FOLDER_NAME
 
-#git add $SITE_FOLDER_NAME
-#git commit -m "Updating ${BRANCH} version docs"
-#git merge -X ours gh-pages
+git checkout $GHPAGES
+git pull
+
+rm -rf $SITE_FOLDER_NAME
+
+mv ../$SITE_FOLDER_NAME .
+git add -A
+git commit -m "Updating ${BRANCH} version docs"
+git push
