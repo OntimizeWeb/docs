7c7ab3cd32da04b803e903e93c251055787dd4d0
Gonzalo Martínez <gonzalo.martinez@imatia.com>
2018-12-03 09:32:49 +0100

    Removing folder

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -12,8 +12,8 @@ git pull
 # Genero carpeta _site
 # bundle exec jekyll build --config _config.yml,_config.deploy.yml
 
-# Renombro _site a vX
-# mv _site $SITE_FOLDER_NAME
+# Renombro _site a ../vX
+# mv _site ../$SITE_FOLDER_NAME
 
 #git add $SITE_FOLDER_NAME
 #git commit -m "Updating ${BRANCH} version docs"
