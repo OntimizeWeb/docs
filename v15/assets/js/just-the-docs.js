9e1529a7b84d16eb57720bf2f62cd21eb960e64e
javier-fernandez-imatia <javier-fernandez-imatia@users.noreply.github.com>
2024-10-02 10:04:00 +0000

    jekyll build from Action f3b4929f0f3445d9dd3e0f56d8e28f4d9319b587

--- v15/assets/js/just-the-docs.js
+++ v15/assets/js/just-the-docs.js
@@ -465,6 +465,10 @@ jtd.setTheme = function(theme) {
 function navLink() {
   var pathname = document.location.pathname;
 
+  if (pathname.endsWith('api')) {
+    pathname = pathname.replace('api', 'overview');
+  }
+
   var navLink = document.getElementById('site-nav').querySelector('a[href="' + pathname + '"]');
   if (navLink) {
     return navLink;
@@ -519,10 +523,12 @@ function activateNav() {
 // Document ready
 
 jtd.onReady(function(){
-  initNav();
+  if (document.getElementById('site-nav')) {
+    initNav();
+    activateNav();
+    scrollNav();
+  }
   initSearch();
-  activateNav();
-  scrollNav();
 });
 
 // Copy button on code
